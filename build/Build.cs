51c4b2992c95c30a88c6d8a39c082a4cfb230408
Eric Carlson <5602927+ericcmmi@users.noreply.github.com>
2021-10-01 15:03:04 -0400

    main and variables

--- build/Build.cs
+++ build/Build.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
@@ -23,17 +24,41 @@ class Build : NukeBuild
     ///   - JetBrains Rider            https://nuke.build/rider
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main()
+    {
+        if (IsLocalBuild)
+        {
+            config = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddUserSecrets<Build>()
+                .Build();
+        }
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+        return Execute<Build>(x => x.Compile);
+    }
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    static IConfiguration config;
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath PublishDirectory => ArtifactsDirectory / "publish";
+    AbsolutePath CommandLineDirectory => PublishDirectory / "exe";
+    AbsolutePath MsBuildDirectory => PublishDirectory / "msbuild";
+    AbsolutePath PowerShellDirectory => PublishDirectory / "powershell";
+    AbsolutePath DeployDirectory => ArtifactsDirectory / "deploy";
+
+    AbsolutePath OutputDirectory => RootDirectory / "Output";
+
+    String AssemblyProduct = "Pickles";
+    String AssemblyCompany = "Pickles";
+    String Version = "3.0.0-alpha.2";
+    String Copyright = "Copyright (c) Jeffrey Cameron 2010-2012, PicklesDoc 2012-present";
+    String NuGetApiKey = "";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -59,5 +84,4 @@ class Build : NukeBuild
                 .SetConfiguration(Configuration)
                 .EnableNoRestore());
         });
-
 }
